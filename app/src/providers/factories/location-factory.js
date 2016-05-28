3d26fd4996157aaafbdc445e0d23ac3089b180bb
max33nau <mtj29@nau.edu>
2016-05-28 12:18:27 -0700

    basic functionality for location service setup

--- app/src/providers/factories/location-factory.js
+++ app/src/providers/factories/location-factory.js
@@ -7,8 +7,58 @@ export default function( ngModule ) {
     var deffered = $q.defer();
     location.defaultLocation = '97205';
     location.getCurrentLocation = function() {
-      
+      var latitude = $rootScope.root.latitude;
+      var longitude = $rootScope.root.longitude;
+      return $http.get(WEATHER_API_URL+'/conditions/lang:EN/q/'+latitude+','+longitude+'.json');
     };
+
+    location.loadData = function(locationData) {
+      var weather = {};
+      weather.currentLocation = locationData.display_location.full;
+      weather.zipcode = locationData.display_location.zip;
+      weather.icon =locationData.icon_url;
+      weather.temperatureF = locationData.temp_f;
+      weather.temperatureC = locationData.temp_c;
+      weather.type = locationData.weather;
+      weather.feelsLikeF = locationData.feelslike_f;
+      weather.feelsLikeC = locationData.feelslike_c;
+      return weather;
+    };
+
+    location.loadForecastData = function(data) {
+      var forecast = {};
+      forecast.days = [];
+      data.simpleforecast.forecastday.forEach(function(day){
+        var dayData = {};
+        dayData.conditions = day.conditions;
+        dayData.dayName = day.date.weekday;
+        dayData.date= day.date.monthname + ' ' + day.date.day + ',' + ' ' + day.date.year;
+        dayData.highF = day.high.fahrenheit;
+        dayData.highC = day.high.celsius;
+        dayData.lowF = day.low.fahrenheit;
+        dayData.lowC = day.low.celsius;
+        forecast.days.push(dayData);
+      });
+      var jj = 0;
+      for(var ii = 0; ii< forecast.days.length; ii++) {
+        var currentDay = data.txt_forecast.forecastday[jj];
+        forecast.days[ii].textF = currentDay.fcttext;
+        forecast.days[ii].textC = currentDay.fcttext_metric;
+        forecast.days[ii].icon = currentDay.icon_url;
+        jj = jj+2;
+      }
+      return forecast;
+    }
+
+
+    location.getLocation = function(zipcode) {
+      return $http.get(WEATHER_API_URL+'/conditions/lang:EN/q/'+zipcode+'.json');
+    };
+
+    location.getForecastZipcode = function(zipcode) {
+      return $http.get(WEATHER_API_URL+'/forecast/lang:EN/q/'+zipcode+'.json');
+    };
+
     return location;
 
  }]);
--- app/src/providers/state-provider/weather-forecast-state/weather-forecast-ctrl.js
+++ app/src/providers/state-provider/weather-forecast-state/weather-forecast-ctrl.js
@@ -3,12 +3,50 @@ export default function(ngModule) {
   ngModule.controller('weatherForecastCtrl', ['$scope','$rootScope','LocationService',function($scope, $rootScope, LocationService) {
     $scope.weather = {};
     $scope.weather.title = 'Weather Forecast';
-    if($rootScope.root.latitude) {
-      console.log('here');
-      LocationService.getCurrentLocation();
+    $scope.weather.currentLocation = 'Portland, OR';
+    if($rootScope.root.latitude && $rootScope.root.longitude) {
+      LocationService.getCurrentLocation().then(function(response){
+        var locationData = response.data.current_observation;
+        $scope.weather = LocationService.loadData(locationData);
+        LocationService.getForecastZipcode($scope.weather.zipcode).then(function(response){
+          var forecastData = LocationService.loadForecastData(response.data.forecast);
+          $scope.weather.today = forecastData.days[0];
+          $scope.weather.tomorrow = forecastData.days[1];
+          $scope.weather.day2 = forecastData.days[2];
+          $scope.weather.day3 = forecastData.days[3];
+        });
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    } else {
+      LocationService.getLocation($scope.currentLocation.zip).then(function(response){
+        var locationData = response.data.current_observation;
+        $scope.weather = LocationService.loadData(locationData);
+        LocationService.getForecastZipcode($scope.weather.zipcode).then(function(response){
+          var forecastData = LocationService.loadForecastData(response.data.forecast);
+          $scope.weather.today = forecastData.days[0];
+          $scope.weather.tomorrow = forecastData.days[1];
+          $scope.weather.day2 = forecastData.days[2];
+          $scope.weather.day3 = forecastData.days[3];
+        });
+      })
+      .catch(function(error){
+        console.log(error);
+      });
     }
     $scope.weather.changeLocation= function() {
-      console.log('change ');
+      LocationService.getLocation($scope.weather.zipcode).then(function(response){
+        var locationData = response.data.current_observation;
+        $scope.weather = LocationService.loadData(locationData);
+        LocationService.getForecastZipcode($scope.weather.zipcode).then(function(response){
+          var forecastData = LocationService.loadForecastData(response.data.forecast);
+          $scope.weather.today = forecastData.days[0];
+          $scope.weather.tomorrow = forecastData.days[1];
+          $scope.weather.day2 = forecastData.days[2];
+          $scope.weather.day3 = forecastData.days[3];
+        });
+      })
     };
   }]);
 }
