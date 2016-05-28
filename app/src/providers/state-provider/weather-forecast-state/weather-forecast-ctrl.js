09a7b127ae7b00060de06ff51350aa0534ac34fe
max33nau <mtj29@nau.edu>
2016-05-28 13:48:19 -0700

    form validation complete

--- app/src/providers/state-provider/weather-forecast-state/weather-forecast-ctrl.js
+++ app/src/providers/state-provider/weather-forecast-state/weather-forecast-ctrl.js
@@ -5,6 +5,7 @@ export default function(ngModule) {
     $scope.weather.error = {};
     $scope.weather.title = 'Weather Forecast';
     $scope.weather.currentLocation = 'Portland, OR';
+    $scope.weather.defaultZipCode = '97267';
     if($rootScope.root.latitude && $rootScope.root.longitude) {
       LocationService.getCurrentLocation().then(function(response){
         $scope.weather = LocationService.loadData(response.data.current_observation);
@@ -16,7 +17,7 @@ export default function(ngModule) {
         console.log(error);
       });
     } else {
-      LocationService.getLocation($scope.currentLocation.zip).then(function(response){
+      LocationService.getLocation($scope.weather.defaultZipCode).then(function(response){
         $scope.weather = LocationService.loadData(response.data.current_observation);
         LocationService.getForecastZipcode($scope.weather.zipcode).then(function(response){
           $scope.weather = LocationService.loadForecastData(response.data.forecast, $scope.weather);
